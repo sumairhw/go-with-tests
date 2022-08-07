7ef1dd673259a2ae2e234f967b5b4562efa17a47
Sumair Hassan <86984915+sumairhw@users.noreply.github.com>
2022-08-07 21:49:12 +0500

    completed hello world of testing

--- hello/hello.go
+++ hello/hello.go
@@ -1,16 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-const prefix = "Hello, "
+const spanish = "Spanish"
+const french = "French"
+const englishPrefix = "Hello, "
+const spanishPrefix = "Hola, "
+const frenchPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return prefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchPrefix
+	case spanish:
+		prefix = spanishPrefix
+	default:
+		prefix = englishPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("John Doe", "Spanish"))
 }
