c8849bb01c28cd9fc7c4619115a0e58d6bffb014
Sumair Hassan <86984915+sumairhw@users.noreply.github.com>
2022-08-09 17:13:02 +0500

    added table driven tests to interfaces ch

--- structs-interfaces/shapes.go
+++ structs-interfaces/shapes.go
@@ -15,6 +15,16 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0
+	// return 0.5 * t.Base * t.Height
+}
+
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
